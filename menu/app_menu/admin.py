ecb20a72529cfd12bdc8317b607b1c1db8a505a9
Владислав <kant1755@mail.ru>
2023-03-05 23:59:30 +0300

    administration

--- menu/app_menu/admin.py
+++ menu/app_menu/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import TreeMenu, TreeMenuCategories
 
-# Register your models here.
+"""
+Register the created models 
+for their processing by the administrator
+"""
+@admin.register(TreeMenu)
+class TreeMenuAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'category', 'link', 'parent',)
+    list_display_links = ('pk', 'name',)
+
+
+@admin.register(TreeMenuCategories)
+class TreeMenuCategoriesAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'systemic_name',)
+    list_display_links = ('pk', 'name',)
