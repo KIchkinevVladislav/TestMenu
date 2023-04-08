ae6eca8b786b794f47b54a0b2ad000a5fa2108e3
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2023-04-08 23:50:58 +0300

    Update urls.py

--- menu/app_menu/urls.py
+++ menu/app_menu/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name='app_menu/main.html'), name='main')
 ]
